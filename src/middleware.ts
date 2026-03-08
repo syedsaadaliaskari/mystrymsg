ec16c266d68c12917981633bcbc525df655826ea
syedsaadaliaskari <syedsaadaliaskari@gmail.com>
2026-03-08 14:49:53 +0500

    Changed the proxy to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-// This function can be marked `async` if using `await` inside
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+  // Use the same secret as your options.ts
   const token = await getToken({
     req: request,
     secret: process.env.AUTH_SECRET,
@@ -11,30 +11,25 @@ export async function proxy(request: NextRequest) {
 
   const url = request.nextUrl;
 
-  // for logged in users
-
+  // 1. If user is logged in and tries to access Auth pages
   if (token) {
-    const AuthPages =
+    if (
       url.pathname === "/" ||
       url.pathname.startsWith("/signIn") ||
       url.pathname.startsWith("/signUp") ||
-      url.pathname.startsWith("/verify");
-
-    if (AuthPages) {
+      url.pathname.startsWith("/verify")
+    ) {
       return NextResponse.redirect(new URL("/dashboard", request.url));
     }
   }
-  // for logout users
 
+  // 2. If user is NOT logged in and tries to access Dashboard
   if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/signIn", request.url));
   }
 
   return NextResponse.next();
 }
-
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
 
 export const config = {
   matcher: ["/signIn", "/signUp", "/", "/verify/:path*", "/dashboard/:path*"],
