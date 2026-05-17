a99ba12b2d5451231243ed6602c63e91dab33bbb
syedsaadaliaskari <syedsaadaliaskari@gmail.com>
2026-05-18 00:18:53 +0500

    ci: add GitHub Actions workflow

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,4 @@
 import { Resend } from "resend";
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
-console.log(`Api key: ${process.env.RESEND_API_KEY}`);
+// console.log(`Api key: ${process.env.RESEND_API_KEY}`);
